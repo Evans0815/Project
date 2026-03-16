a905b38026cd975631a40764ec9b1fe6513a27fa
evans <evans_dev@163.com>
2026-03-16 16:46:07 +0800

    add ServerApplication

---
+++ server/src/main/java/com/evans/project/server/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.evans.project.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author evans
+ * @date 2026/3/16
+ */
+@SpringBootApplication
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
